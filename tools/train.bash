34bbccc8dfb434647c5d6a983c5ec33823922504
Hide Inada <hideyuki@gmail.com>
2019-02-22 13:01:09 -0800

    Added train.bash

---
+++ tools/train.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Replace the directory name after the --image_dir
+
+export TFHUB_CACHE_DIR=/tmp/food101/module_cache
+
+time python retrain.py --image_dir=../../../../dataset/food101/food-101/images
+
