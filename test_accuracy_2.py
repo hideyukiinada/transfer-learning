7848a925f1e3f9c3e760c2750cc199c3e6fce12e
Hide Inada <hideyuki@gmail.com>
2019-02-22 19:43:49 -0800

    test accuracy 2 optimized.

--- test_accuracy_2.py
+++ test_accuracy_2.py
@@ -30,6 +30,8 @@ from pathlib import Path
 import numpy as np
 import tensorflow as tf
 
+RESULT_FILE = "accuracy_result.txt"
+
 log = logging.getLogger(__name__)
 logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))  # Change the 2nd arg to INFO to suppress debug logging
 
@@ -83,41 +85,35 @@ def load_labels(label_file):
     return label
 
 
-def predict(model_file=None, label_file=None, input_layer=None, output_layer=None, file_name=None):
+def predict(sess=None, graph=None, input_operation=None, output_operation=None, label_file_name=None,
+            image_file_name=None):
     input_height = 299
     input_width = 299
     input_mean = 0
     input_std = 255
 
-    graph = load_graph(model_file)
     t = read_tensor_from_image_file(
-        file_name,
+        image_file_name,
         input_height=input_height,
         input_width=input_width,
         input_mean=input_mean,
         input_std=input_std)
 
-    input_name = "import/" + input_layer
-    output_name = "import/" + output_layer
-    input_operation = graph.get_operation_by_name(input_name)
-    output_operation = graph.get_operation_by_name(output_name)
-
-    with tf.Session(graph=graph) as sess:
-        results = sess.run(output_operation.outputs[0], {
-            input_operation.outputs[0]: t
-        })
+    results = sess.run(output_operation.outputs[0], {
+        input_operation.outputs[0]: t
+    })
     results = np.squeeze(results)
 
     top_k = results.argsort()[-5:][::-1]
-    labels = load_labels(label_file)
+    labels = load_labels(label_file_name)
 
     top_labels = list()
     for i in top_k:
         top_labels.append(labels[i])
-        # print(labels[i], results[i])
 
     top_labels = list(map(lambda e: e.replace(" ", ""), top_labels))
 
+    # print(top_labels)
     return top_labels
 
 
@@ -131,54 +127,64 @@ def main():
     if image_base_dir_path.exists() is False:
         raise ValueError("Image base directory is not found.")
 
-    with open("accuracy_result.txt", "w") as f_out:
+    model_file_name = "/tmp/output_graph.pb"
+    label_file_name = "/tmp/output_labels.txt"
+    input_layer = "Placeholder"
+    output_layer = "final_result"
 
-        reg = re.compile("[ |[0-9]|\.")
+    graph = load_graph(model_file_name)
 
-        score = dict()
-        for i, d in enumerate([d for d in image_base_dir_path.iterdir() if d.is_dir()]):
+    input_name = "import/" + input_layer
+    output_name = "import/" + output_layer
+    input_operation = graph.get_operation_by_name(input_name)
+    output_operation = graph.get_operation_by_name(output_name)
 
-            if i > 0:
-                break
+    with tf.Session(graph=graph) as sess:
 
-            log.info("Processing " + str(d))
+        with open(RESULT_FILE, "w") as f_out:
 
-            normalized_directory_name = d.name.lower().replace("_", "")
-            print("Dir: " + normalized_directory_name)
-            score[normalized_directory_name] = dict()
+            score = dict()
+            for i, d in enumerate([d for d in image_base_dir_path.iterdir() if d.is_dir()]):
 
-            score[normalized_directory_name]["total"] = 0
-            score[normalized_directory_name]["top5"] = 0
-            score[normalized_directory_name]["top1"] = 0
+                if i > 0:
+                    break
 
-            for f in d.glob("*.jpg"):
-                score[normalized_directory_name]["total"] += 1
+                log.info("Processing " + str(d))
 
-                log.info("Processing " + str(f))
+                normalized_directory_name = d.name.lower().replace("_", "")
+                print("Dir: " + normalized_directory_name)
+                score[normalized_directory_name] = dict()
 
-                top5 = predict(model_file="/tmp/output_graph.pb",
-                                         label_file="/tmp/output_labels.txt",
-                                         input_layer="Placeholder",
-                                         output_layer="final_result",
-                                         file_name=str(f))
+                score[normalized_directory_name]["total"] = 0
+                score[normalized_directory_name]["top5"] = 0
+                score[normalized_directory_name]["top1"] = 0
 
-                # print(top5)
+                for f in d.glob("*.jpg"):
+                    score[normalized_directory_name]["total"] += 1
 
-                if normalized_directory_name in top5:
-                    log.info("In top5!")
-                    score[normalized_directory_name]["top5"] += 1
+                    log.info("Processing " + str(f))
 
-                if normalized_directory_name == top5[0]:
-                    score[normalized_directory_name]["top1"] += 1
+                    top5 = predict(sess=sess,
+                                   graph=graph,
+                                   input_operation=input_operation,
+                                   output_operation=output_operation,
+                                   label_file_name=label_file_name,
+                                   image_file_name=str(f))
 
-            score[normalized_directory_name]["top5_accuracy"] = score[normalized_directory_name]["top5"] / \
-                                                                score[normalized_directory_name]["total"]
-            score[normalized_directory_name]["top1_accuracy"] = score[normalized_directory_name]["top1"] / \
-                                                                score[normalized_directory_name]["total"]
+                    if normalized_directory_name in top5:
+                        score[normalized_directory_name]["top5"] += 1
 
-            f_out.write("%s\t%0.4f\t%0.4f" % (
-                normalized_directory_name, score[normalized_directory_name]["top1_accuracy"],
-                score[normalized_directory_name]["top5_accuracy"]))
+                    if normalized_directory_name == top5[0]:
+                        score[normalized_directory_name]["top1"] += 1
+
+                score[normalized_directory_name]["top5_accuracy"] = score[normalized_directory_name]["top5"] / \
+                                                                    score[normalized_directory_name]["total"]
+                score[normalized_directory_name]["top1_accuracy"] = score[normalized_directory_name]["top1"] / \
+                                                                    score[normalized_directory_name]["total"]
+
+                f_out.write("%s\t%0.4f\t%0.4f" % (
+                    normalized_directory_name, score[normalized_directory_name]["top1_accuracy"],
+                    score[normalized_directory_name]["top5_accuracy"]))
 
 
 if __name__ == "__main__":
